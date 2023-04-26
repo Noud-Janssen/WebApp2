43a0ffaaa7dbbcc52d169f13e9de692cf3918e3f
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-04-26 12:42:08 +0200

    Update nav

--- php/nav.php
+++ php/nav.php
@@ -1,3 +1,14 @@
+<svg id="nav-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0099ff" fill-opacity="1" d="M0,288L80,250.7C160,213,320,139,480,138.7C640,139,800,213,960,245.3C1120,277,1280,267,1360,261.3L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+    </svg>
 <nav>
-    
+    <h1>
+        Discount was Here
+    </h1>
+    <div class="link-row">
+        <a href="">Home</a>
+        <a href="">Over ons</a>
+        <a href="">Contact</a>
+        <a href="">Login</a>
+    </div>
 </nav>
