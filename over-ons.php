be2be59c18749b36d265a0934585a7bbe8620a98
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-05-11 10:17:07 +0200

    Revert "Merge branch 'main' of https://github.com/Noud-Janssen/WebApp2"
    This reverts commit 4c8debdc1e6dd4f74548d5526a30677cbb8357dc, reversing
    changes made to 398c79bee96cb6ccf226d2ae77213449a6502fc0.

--- over-ons.php
+++ over-ons.php
@@ -16,24 +16,6 @@
     <?php
     require_once('php/nav.php')
     ?>
-    <div class="over-ons-container">
-        <h1 id="over-ons-titel">Over Ons</h1>
-        <p>Welkom bij onze reisorganisatie! Wij zijn een gepassioneerd team van reisexperts die er alles aan doen om uw droomreis werkelijkheid te maken.</p>
-        <br>
-        <p>Onze organisatie is opgericht met als doel reizigers te helpen de wereld te verkennen en onvergetelijke ervaringen te beleven. Wij zijn ervan overtuigd dat reizen ons de mogelijkheid biedt om nieuwe culturen te ontdekken, nieuwe mensen te ontmoeten en om te leren van de prachtige diversiteit die onze planeet te bieden heeft.</p>
-        <br>
-        <p>Onze reisexperts hebben uitgebreide ervaring in de reisbranche en zijn altijd op zoek naar de nieuwste trends en bestemmingen. Wij werken nauw samen met lokale partners en gidsen om ervoor te zorgen dat onze reizigers de meest authentieke ervaringen krijgen en in contact komen met de lokale bevolking.</p>
-        <br>
-        <p>Wij geloven in verantwoord toerisme en willen bijdragen aan een duurzamere wereld. Daarom streven we ernaar om onze reizen zo milieuvriendelijk mogelijk te maken en ondersteunen we lokale projecten die bijdragen aan de ontwikkeling van de gemeenschappen die we bezoeken.</p>
-        <br>
-        <p>Onze klantenservice is van topklasse en we staan altijd klaar om al uw vragen te beantwoorden en u te helpen bij het plannen van uw perfecte reis. We bieden op maat gemaakte reizen aan die aansluiten bij uw individuele wensen en behoeften, zodat u zich volledig kunt ontspannen en genieten van uw vakantie.</p>
-        <br>
-        <p>Of u nu op zoek bent naar een ontspannen strandvakantie, een avontuurlijke rondreis, of een luxe cruise, bij ons bent u aan het juiste adres. Wij zijn er om uw droomreis werkelijkheid te maken en u de tijd van uw leven te bezorgen.</p>
-        <br>
-        <p>Bedankt voor uw interesse in onze reisorganisatie en we hopen u binnenkort te mogen verwelkomen op een van onze reizen!</p>
-        <br>
-        <p>Wilt u contact met ons opnemen ga dan naar <a id="over-to-contact-link" href="contact.php">hier</a>.</p>
-    </div>
 </body>
 
 </html>
