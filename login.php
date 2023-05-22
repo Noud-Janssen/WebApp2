7e3bed3332c74ccb19f9dd1d65557ece48f36f7e
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-05-22 13:35:50 +0200

    login

--- login.php
+++ login.php
@@ -10,20 +10,41 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,200;0,400;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/noud.css">
 </head>
 
 <body>
     <?php
-    require_once('php/nav.php')
+    require_once('php/config.php');
+    require_once('php/nav.php');
+
+    $errorMessage = "";
+
+    if (isset($_POST['login'])) {
+        $resultSet = $conn->prepare("SELECT * FROM accounts WHERE email = ?");
+        $resultSet->execute([$_POST['email']]);
+        if ($result = $resultSet->fetch()) {
+            if ($_POST['password'] == $result['password']) {
+                $_SESSION['inlogid'] = $result['id'];
+                header("Location: index.php");
+            } else {
+                $errorMessage = "Verkeer wachtwoord";
+            }
+        } else {
+            $errorMessage = "Account niet gevonden";
+        }
+        
+    }
     ?>
 </body style="">
-    <form action="post" id="loginForm">
+    <form action="" method="post" id="loginForm">
         <h2>E-mail</h2>
-        <input type="text" name="email" id="">
+        <input type="text" name="email" id="" required>
         <h2>Password</h2>
-        <input type="password" name="password" id="">
+        <input type="password" name="password" id="" required>
         <h2></h2>
-        <input type="submit" value="login">
+        <input type="submit" name="login" value="login">
+        <?php echo "<h3 id='errorLogin'>".$errorMessage."</h3>"?>
         <a href="">Maak account</a>
     </form>
 </html>
