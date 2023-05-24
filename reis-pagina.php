cb494c94364b0f653213423d53f1a55816753910
Floris van Drunen <117982402+wontis123@users.noreply.github.com>
2023-05-24 10:03:10 +0200

    reis pagina& voorwaarden

--- reis-pagina.php
+++ reis-pagina.php
@@ -15,36 +15,50 @@
 
 <body>
     <?php
-    require_once('php/nav.php')
-    ?>
+    require('php/config.php');
+    require_once('php/nav.php');
+    $resultSet = $conn->prepare("SELECT * FROM reizen WHERE id = ?");
+    $resultSet->execute([$_GET['id']]);
+    $result = $resultSet->fetch();
+    if($result['handicap_vriendelijk']==0){
+        $handicapresult="nee";
+    }else{
+        $handicapresult="ja";
+    }
+    echo '
     <div class="reis-pagina-container">
-        <div class="reis-foto-container"></div>
-        <div class="reis-info">
-            <h1 id="naam-plek">Nederland, Nijmegen.</h1>
-            <h2>Reis Planning: 29 feb tot 32 maart</h2>
-            <ul>
-                <h2 id="head-lijst">Informatie.</h2>
-                <li>wc's: 3.</li>
-                <li>slaapkamers: 3.</li>
-                <li>handikap vriendelijk: is het helaas niet.</li>
-            </ul>
-            <ul>
-                <h2 id="head-lijst">Leuke dingen in de buurt.</h2>
-                <li>Fiets routen.</li>
-                <li>Wandelen.</li>
-                <li>Naar het Centrum.</li>
-                <li>Voor een bus of trein springen.</li>
-            </ul>
-        </div>
-        <div class="reis-recensies">
-            <ul>
-                <h2 id="head-lijst">Recensies.</h2>
-                <li>★★★★: Leuke activiteiten om te doen en leuke plek waar je overnacht.</li>
-                <li>★★: Niks leuks aan, maar voor een bus springen is wel een beetje leuk teminste.</li>
-                <li>★★★: Prima maar niks bijzonders. Ook best veel lawaai.</li>
-            </ul>
-        </div>
+    <div class="reis-foto-container"></div>
+    <div class="reis-info">
+    <div class="naam-prijs-balk">
+    <h1 id="naam-plek">' . $result['land'] . ', ' . $result['plaats'] . '</h1>
+    <h1 id="prijs">€' . $result['preis'] . '</h1>
+</div>
+        <h2>Reis Planning: ' . $result['vertrekDatum'] . ' tot ' . $result['terugkomstDatum'] . '</h2>
+        <ul>
+            <h2 id="head-lijst">Informatie.</h2>
+            <li>wc: ' . $result['wc'] . '.</li>
+            <li>slaapkamers: ' . $result['slaapkamers'] . '.</li>
+            <li>handikap vriendelijk: ' . $handicapresult . '.</li>
+            <li>oppervlakte woning: ' . $result['oppervlakte_woning'] . 'm².</li>
+        </ul>
+        <ul>
+            <h2 id="head-lijst">Beschrijving.</h2>
+            <li>' . $result['beschrijving'] . '</li>
+        </ul>
     </div>
+    <div class="reis-recensies">
+        <ul>
+            <h2 id="head-lijst">Recensies.</h2>
+            <li>★★★★: Leuke activiteiten om te doen en leuke plek waar je overnacht.</li>
+            <li>★★: Niks leuks aan, maar voor een bus springen is wel een beetje leuk teminste.</li>
+            <li>★★★: Prima maar niks bijzonders. Ook best veel lawaai.</li>
+        </ul>
+    </div>
+    </div>'
+    ?>
+
+
+
 </body>
 
 </html>
---
+++ voorwaarden.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,200;0,400;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/floris.css">
+</head>
+
+<body>
+    <?php
+    require_once('php/nav.php')
+    ?>
+    <div class="over-ons-bg-img">
+        <div class="over-ons-container">
+            <h1 id="over-ons-titel">Algemene Voorwaarden</h1>
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
