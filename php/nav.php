7ef62a54eb4d2dca06ef33cf11186ef77752eed8
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-05-25 12:25:54 +0200

    Update login

--- php/nav.php
+++ php/nav.php
@@ -8,13 +8,20 @@
         <a href="contact.php">Contact</a>
         <a href="voorwaarden.php">Voorwaarden</a>
         <?php 
-            if (isset($_SESSION['inlogid'])) {
+            if (isset($_POST['logout'])) {
+                session_destroy();
+                header('index.php');
+                echo "<a href='login.php'>Login</a>";
+            } else if (isset($_SESSION['inlogid'])) {
                 echo "<div id='accountButton'>Account
                 <div id='accountOptions'>
                     <h3>Account</h3>
-                    <a href='mijn-reizen.php'>Mijn reizen</a>
+                    <a href='mijn-reizen.php'>Mijn Boekingen</a>
                     <a href='mijn-account.php'>Mijn account</a>
-                    <a href='logout.php'>Logout</a>
+                    <form method='post'>
+                    <input type='submit' name='logout' value='Logout'>
+                    </form>
+                    
                 </div>
                 </div>";
             } else {
@@ -22,5 +29,6 @@
             }
         ?>
     </div>
+    
     
 </nav>
--- php/search.php
+++ php/search.php
@@ -1,6 +1,20 @@
 <form class="searchbalk" action="resultaat.php">
+    <div class="inputWrapper">
+        <h3>Waarheen?</h3>
         <input type="text" name="land" id="landform">
-        <input type="date" name="vertrekdatum" class="vertrekdatum" id="vertrek-datum">
-        <input type="date" name="terugkomstdatum" class="vertrekdatum" id="aankomst-datum">
+    </div>
+    <div class="inputWrapper">
+        <h3>Vertrekken tussen: </h3>
+        <input type="date" name="vertrekdatum1" class="vertrekdatum" id="vertrek-datum">
+        <input type="date" name="vertrekdatum2" class="vertrekdatum" id="vertrek-datum">
+    </div>
+    <div class="inputWrapper">
+        <h3>Terugkomen tussen: </h3>
+        <input type="date" name="terugkomstdatum1" class="vertrekdatum" id="aankomst-datum">
+        <input type="date" name="terugkomstdatum2" class="vertrekdatum" id="aankomst-datum">
+    </div>
+    <div class="inputWrapper"></div>
+    <div class="inputWrapper">
         <input type="submit" value="zoek" id="zoek-knop">
+    </div>
     </form>
